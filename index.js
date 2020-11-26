52d3276bba1797f16cbdf653e29aeb8fe32e13ce
Kenny Kim <kennykim11@gmail.com>
2020-11-26 00:46:07 -0500

    now with a flying plane!

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-var img = new Image()
+var img = new Image(47.7398, 47.7398)
 img.src = "planeicon.svg"
 
 var time = 0
 const timeInterval = 10
 var paused = false
 var pulses = []
-var plane = {x: 200, y: 200}
+var plane = {x: 200, y: 200, v: 0.5, heading: -Math.PI/2}
 var emitter = {x: 100, y: 75}
+var alerted = false
+var hitCooldown = 0
+const hitCooldownStart = 500
 const pulseInterval = 1000
 const radiusGrowth = 1
 const radiusTimeout = 2000
+const planeVMax = 1
 
 function checkCollision(plane, pulse) {
     const deltaPulseToPlane = Math.abs(Math.sqrt((plane.x - pulse.startX)**2 + (plane.y - pulse.startY)**2) - pulse.radius)
-    if (deltaPulseToPlane < 0.7) return true
+    if (deltaPulseToPlane < 0.9) return true
+}
+
+function rotateAndPaintImage(context, image, angleInRad, positionX, positionY) {
+    context.save()
+    context.translate(positionX, positionY)
+    context.rotate(angleInRad);
+    context.drawImage(image, -img.width/2, -img.height/2);
+    context.restore()
 }
 
 function redraw(canvas) {
     const cv = canvas.getContext("2d");
     cv.clearRect(0, 0, canvas.width, canvas.height);
+    rotateAndPaintImage(cv, img, plane.heading + Math.PI/2, plane.x, plane.y)
 
     if (!(time % pulseInterval)){ //Make a new pulse if time
         pulses.push({
@@ -36,10 +49,45 @@ function redraw(canvas) {
         cv.arc(pulse.startX, pulse.startY, pulse.radius, 0, 2 * Math.PI);
         cv.stroke();
 
-        if (checkCollision(plane, pulse)) console.log('HIT')
-
+        if (hitCooldown) hitCooldown -= 1
+        if (checkCollision(plane, pulse) && !hitCooldown) {
+            console.log('HIT')
+            hitCooldown = hitCooldownStart
+        }
         pulse.radius += 1 * radiusGrowth
     }
+}
+
+function movePlane(canvas){
+    plane.x += plane.v * Math.cos(plane.heading)
+    plane.y += plane.v * Math.sin(plane.heading)
+    if (!alerted && (plane.x < 0 || plane.x > canvas.width || plane.y < 0 || plane.y > canvas.height)){
+        alerted = true
+        alert("Out of bounds")
+    } 
+}
+
+function setUpKeys(){
+    document.addEventListener('keydown', function(event) {
+        switch (event.key){
+            case "ArrowDown":
+            case "S":
+                if (plane.v > 0.2) plane.v -= 0.1
+                break
+            case "ArrowUp":
+            case "W":
+                if (plane.v < planeVMax) plane.v += 0.1
+                break
+            case "ArrowRight":
+            case "D":
+                plane.heading += 0.07
+                break
+            case "ArrowLeft":
+            case "A":
+                plane.heading -= 0.07
+                break
+        }
+    });
 }
 
 window.onload = function(){
@@ -48,15 +96,15 @@ window.onload = function(){
     canvas.width = +canvasStyle.width.slice(0, -2)
     canvas.height = +canvasStyle.height.slice(0, -2)
     var cv = canvas.getContext("2d");
-    console.log(canvas)
     cv.fillStyle = "white";
-    cv.fillRect(0, 0, canvas.width, canvas.height);
-    cv.drawImage(img, 10, 10)
+
+    setUpKeys()
 
     this.timerHandler = setInterval(function(){
         if (!paused) {
-            time += timeInterval;
+            movePlane(canvas)
             redraw(canvas);
+            time += timeInterval;
         }
     },timeInterval)
  };
